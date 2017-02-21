88f0f829bbf4422ea5d29787fb1839edf5e3b659
Payam Naderi <naderi.payam@gmail.com>
2017-02-21 14:51:46 +0330

    remionder

--- .docker-compose/todo.php
+++ .docker-compose/todo.php
@@ -1,2 +1,3 @@
 <?php
 // TODO setup iptable to protect services ports access to the world; such as 8000, 32800
+// TODO define upstream on frontproxy enabled site nginx config for each service instead name
