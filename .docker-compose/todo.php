67008d8905100815d62194b15de71388824eab01
Payam Naderi <naderi.payam@gmail.com>
2017-04-22 14:30:26 +0430

    implement content post upstream proxy

--- .docker-compose/todo.php
+++ .docker-compose/todo.php
@@ -1,3 +1,2 @@
 <?php
 // TODO setup iptable to protect services ports access to the world; such as 8000, 32800
-// TODO define upstream on frontproxy enabled site nginx config for each service instead name
